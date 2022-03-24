f925c8abed9b84a71dc99fb22bd16a5fc57bea8e
zaw83 <67044969+zaw83@users.noreply.github.com>
2022-03-25 00:31:33 +0630

    Blazor Day2 Night Update

---
+++ BlazorApplication/BlazorServer/Pages/CounterBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        public int currentCount = 0;
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
